c805b2ffc918119eabe18afac599d485cc3472bd
kreciorek <krecior1337@proton.me>
2025-07-06 21:15:42 +0000

    msm8916: uprev camera.provider

--- msm8916.mk
+++ msm8916.mk
@@ -97,8 +97,8 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.5-impl \
+    android.hardware.camera.provider@2.5-service \
     libcamera_shim \
     camera.msm8916 \
     GcamGo
