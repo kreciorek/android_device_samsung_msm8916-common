9211b4001c44ef090b8a8f3a6af51a599889b2d2
kreciorek <krecior1337@proton.me>
2025-08-02 20:26:30 +0000

    Revert "msm8916-common: sepolicy: removed custom sepolicies & using qcom ones"
    This reverts commit 3fa92745435774575a83909cfd342d988ebf4c5b.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -256,7 +256,7 @@ endif
 #endif
 
 # SELinux
-#include device/qcom/sepolicy-legacy/sepolicy.mk
+include device/qcom/sepolicy-legacy/sepolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += device/samsung/msm8916-common/sepolicy/vendor
 #PLAT_PRIVATE_POLICY += device/samsung/msm8916-common/sepolicy/private
